703171affa1561a70596e0367e6fdbc6f428fae6
MrLad01 <ladayodeji@gmail.com>
2024-10-02 22:02:16 +0100

    debug

--- backend/utils/filterTasksByCompany.js
+++ backend/utils/filterTasksByCompany.js
@@ -4,7 +4,9 @@ const User = require('../Models/User');
 // Middleware to filter tasks based on the logged-in user's company_name
 const filterTasksByCompany = async (data, companyName) => {
 
-    const mappedUsers = data.map(item => User.findById(item.assigned_user._id).company_name)
+    const mappedUsers = data.map(item => User.findById(item.assigned_user._id))
+    console.log(data);
+    
 
     console.log(mappedUsers);
     
